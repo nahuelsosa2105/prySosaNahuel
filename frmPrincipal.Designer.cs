2f951c0fd77d8d9fa9f6fa55eb017e809aaf33b2
nahuelsosa2105 <nahuelsosa2105@gmail.com>
2024-04-05 20:58:57 -0300

    Cambios en la interfaz grafica Principal con acceso a las diferentes pantallas

--- frmPrincipal.Designer.cs
+++ frmPrincipal.Designer.cs
@@ -28,20 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.tbPrincipal = new MaterialSkin.Controls.MaterialTabControl();
+            this.tbpInicio = new System.Windows.Forms.TabPage();
+            this.tbpFirma = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tbpNave = new System.Windows.Forms.TabPage();
+            this.tbPrincipal.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tbPrincipal
+            // 
+            this.tbPrincipal.Controls.Add(this.tbpInicio);
+            this.tbPrincipal.Controls.Add(this.tbpFirma);
+            this.tbPrincipal.Controls.Add(this.tbpNave);
+            this.tbPrincipal.Depth = 0;
+            this.tbPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbPrincipal.ImageList = this.imageList1;
+            this.tbPrincipal.Location = new System.Drawing.Point(3, 64);
+            this.tbPrincipal.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tbPrincipal.Multiline = true;
+            this.tbPrincipal.Name = "tbPrincipal";
+            this.tbPrincipal.SelectedIndex = 0;
+            this.tbPrincipal.Size = new System.Drawing.Size(794, 383);
+            this.tbPrincipal.TabIndex = 1;
+            // 
+            // tbpInicio
+            // 
+            this.tbpInicio.BackColor = System.Drawing.Color.White;
+            this.tbpInicio.ImageKey = "inicio.jpg";
+            this.tbpInicio.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.tbpInicio.Location = new System.Drawing.Point(4, 39);
+            this.tbpInicio.Name = "tbpInicio";
+            this.tbpInicio.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpInicio.Size = new System.Drawing.Size(786, 340);
+            this.tbpInicio.TabIndex = 0;
+            this.tbpInicio.Text = "Inicio";
+            // 
+            // tbpFirma
+            // 
+            this.tbpFirma.BackColor = System.Drawing.Color.White;
+            this.tbpFirma.ImageKey = "firma.png";
+            this.tbpFirma.Location = new System.Drawing.Point(4, 39);
+            this.tbpFirma.Name = "tbpFirma";
+            this.tbpFirma.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpFirma.Size = new System.Drawing.Size(786, 340);
+            this.tbpFirma.TabIndex = 1;
+            this.tbpFirma.Text = "Firma";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.White;
+            this.imageList1.Images.SetKeyName(0, "firma.png");
+            this.imageList1.Images.SetKeyName(1, "Nave Galaga Recorte.png");
+            this.imageList1.Images.SetKeyName(2, "inicio.jpg");
+            // 
+            // tbpNave
+            // 
+            this.tbpNave.BackColor = System.Drawing.Color.White;
+            this.tbpNave.ImageKey = "Nave Galaga Recorte.png";
+            this.tbpNave.Location = new System.Drawing.Point(4, 39);
+            this.tbpNave.Name = "tbpNave";
+            this.tbpNave.Size = new System.Drawing.Size(786, 340);
+            this.tbpNave.TabIndex = 2;
+            this.tbpNave.Text = "Juego tipo \"Galaga\"";
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbPrincipal);
+            this.DrawerShowIconsWhenHidden = true;
+            this.DrawerTabControl = this.tbPrincipal;
             this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu Principal";
+            this.tbPrincipal.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialTabControl tbPrincipal;
+        private System.Windows.Forms.TabPage tbpInicio;
+        private System.Windows.Forms.TabPage tbpFirma;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TabPage tbpNave;
     }
 }
 
--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin.Controls;
 
 namespace prySosaNahuel
 {
-    public partial class frmPrincipal : Form
+    public partial class frmPrincipal : MaterialForm
     {
+        readonly MaterialSkin.MaterialSkinManager materialSkinManager;
         public frmPrincipal()
         {
             InitializeComponent();
+            materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
+            materialSkinManager.EnforceBackcolorOnAllComponents = true;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Indigo500, MaterialSkin.Primary.Indigo700, MaterialSkin.Primary.Indigo100, MaterialSkin.Accent.Pink200, MaterialSkin.TextShade.WHITE);
+            
         }
     }
 }
